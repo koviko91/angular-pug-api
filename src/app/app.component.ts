3d165838b24cd144a8a6e256be1892fa85d90f88
Kovats Oliver <koviko@gmail.com>
2018-04-11 15:33:22 +0200

    angular-connection-ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
 import { Component } from '@angular/core';
-
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import { HttpClient } from 'selenium-webdriver/http';
+/* import { Observable } from 'rxjs/Observable'; */
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.pug',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  /*adat: Todo;
+   modal: Todo;
+   datas: Array<Todo>;*/
+  adat: object = {
+    id: "",
+    important: "",
+    done: "",
+    text: "",
+    color: ""
+  }
+  modal: object = {
+    id: "",
+    important: "",
+    done: "",
+    text: "",
+    color: ""
+  }
+  datas: any;
+  constructor(public http: Http) {
+    this.getAll();
+  }
+
+  getAll() {
+    this.http.get('http://localhost:3000/users').subscribe(
+      data => this.datas = JSON.parse(data['_body'])
+    );
+  }
+
+  create() {
+    this.http.post('http://localhost:3000/users/', this.adat).subscribe(
+      () => this.getAll());
+
+  }
+  update() {
+    this.http.put('http://localhost:3000/users/' + this.modal['id'], this.modal)
+      .subscribe(() => this.getAll());
+  }
+
+  deleteRow(id) {
+    this.http.delete('http://localhost:3000/users/' + id)
+      .subscribe(() => this.getAll());
+  }
+  modalChange(id) {
+    let choosen = this.datas.filter(item => item.id == id)[0];
+    // this.modal = this.datas.filter(item => item.id == id)[0];
+    this.modal['id'] = choosen.id;
+    this.modal['important'] = choosen.important;
+    this.modal['done'] = choosen.done;
+    this.modal['text'] = choosen.text;
+    this.modal['color'] = choosen.color;
+  }
 }
+
+interface Todo {
+  id?: number;
+  important?: string;
+  done?: string;
+  text: string;
+  color?: string
+}
+//It's Works!!!
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,19 @@
+/* use PUG
+  ng new todo-frontend
+  ng eject
+  npm i pug pug-html-loader -D
+  webpack.config js rules kiegészítése:
+  {
+        "test": /\.pug$/,
+        "loader": ["raw-loader", 'pug-html-loader']
+      },
+app.component.html átnevezése .pug -ra , app.component.ts -ben is átírnia  tempalte url-t
+npm start
+*/
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +23,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
